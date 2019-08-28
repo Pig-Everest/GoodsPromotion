7844aeb846927f5e6de883615f18439eb4e91b45
twl-GitHub <1225810912@qq.com>
2019-08-28 14:10:09 +0800

    怎么加多买促销界面

--- src/main/java/com/haut/promotion/mapper/PromotionstyleMapper.java
+++
@@ -1,8 +0,0 @@
-package com.haut.promotion.mapper;
-
-import com.haut.promotion.domain.Promotionstyle;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface PromotionstyleMapper extends Mapper<Promotionstyle> {
-
-}
