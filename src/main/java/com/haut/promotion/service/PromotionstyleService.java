744b4d92a984ee682a72229714a8a9a41e9fc78b
twl-GitHub <1225810912@qq.com>
2019-08-28 14:26:06 +0800

    增加多买促销界面

---
+++ src/main/java/com/haut/promotion/service/PromotionstyleService.java
@@ -0,0 +1,27 @@
+package com.haut.promotion.service;
+
+import com.haut.promotion.domain.Promotionstyle;
+
+import java.util.List;
+
+public interface PromotionstyleService{
+
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Promotionstyle record);
+
+    int insertSelective(Promotionstyle record);
+
+    Promotionstyle selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Promotionstyle record);
+
+    int updateByPrimaryKey(Promotionstyle record);
+
+    /*
+    * 查询所有促销类型
+    * */
+     public List<Promotionstyle> selectAllPromotionStyle();
+
+}
---
+++ src/main/java/com/haut/promotion/service/impl/PromotionstyleServiceImpl.java
@@ -0,0 +1,54 @@
+package com.haut.promotion.service.impl;
+
+import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
+import com.haut.promotion.domain.Promotionstyle;
+import com.haut.promotion.mapper.PromotionstyleMapper;
+import com.haut.promotion.service.PromotionstyleService;
+
+import java.util.List;
+
+@Service
+public class PromotionstyleServiceImpl implements PromotionstyleService{
+
+    @Resource
+    private PromotionstyleMapper promotionstyleMapper;
+
+    @Override
+    public int deleteByPrimaryKey(Integer id) {
+        return promotionstyleMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public int insert(Promotionstyle record) {
+        return promotionstyleMapper.insert(record);
+    }
+
+    @Override
+    public int insertSelective(Promotionstyle record) {
+        return promotionstyleMapper.insertSelective(record);
+    }
+
+    @Override
+    public Promotionstyle selectByPrimaryKey(Integer id) {
+        return promotionstyleMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public int updateByPrimaryKeySelective(Promotionstyle record) {
+        return promotionstyleMapper.updateByPrimaryKeySelective(record);
+    }
+
+    @Override
+    public int updateByPrimaryKey(Promotionstyle record) {
+        return promotionstyleMapper.updateByPrimaryKey(record);
+    }
+
+    @Override
+    /*
+     * 查询所有促销类型
+     * */
+   public  List<Promotionstyle> selectAllPromotionStyle(){
+      return promotionstyleMapper.selectAll();
+    }
+}
